ccc7cda4e415db06c9d959955fa4619d5c316beb
Christina Cornell <cdhorn515@gmail.com>
2017-07-15 12:08:36 -0400

    creating password hash test is passing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 const mongoose = require('mongoose');
 const User = require('./models/user');
+const crypto = require('crypto');
+
 
 mongoose.Promise = require('bluebird');
 
@@ -11,13 +13,15 @@ const mongoURL = require('./config.json')[env].mongoURL;
 mongoose.connect(mongoURL);
 
 const createUser = function(username, password) {
-  return User.create({username: username, password: password});
+  return User.create({username: username, password: createPasswordHashObj(password)});
 };
 
 const createPasswordHashObj = function(password){
  const hash = crypto.pbkdf2Sync(password, 'asdf', 100, 512, 'sha512');
  const hashString = hash.toString('base64');
+ // console.log("hashString: ", hashString);
  return {salt: 'asdf', iterations: 100, hash: hashString};
+
 };
 
 module.exports = {
--- tests/modelTests.js
+++ tests/modelTests.js
@@ -1,6 +1,7 @@
-
+const User = require('../models/user');
 const createUser = require('../app').createUser;
-
+const createPasswordHashObj = require('../app').createPasswordHashObj;
+// const hashString = require('../app').hashString;
 
 const expect = require('chai').expect;
 
@@ -10,18 +11,21 @@ describe('use model tests', function(){
 
   it('can generate a password hash from a string', function(done) {
     const passwordObj = createPasswordHashObj('password');
-      expect(passwordObj).to.not.equal(null);
-    const expectedHashObj = {salt: 'asdf', iterations: 100, hash: hashString};
+    const expectedHashObj = {salt: 'asdf', iterations: 100, hash: "hPzzhHUWenNASgMt0ddX+4FtAg6+yDQ4n5ZPUxUqVrigmoCd0nafGqGISjhqoRuucPMQOhGhJkjJh80Z5bY73Ph+wgL+l8qwvo6snlpqZqvG9Q3rkFviV7tWSbQzd1Ub5mO5ZHhhvL11d2W8odJAqpg2E/NVtKqK9bn0znIQHEQh6uRbemhlj8UQQ7TF9mFZmdYKmC1BSbLBjzkm95sLs5k7jqpyOlscDMOKnLOEbDpDeQi89hIAaEjQ0porRiuKCNTGLHgc8PGD+hY4lmmI2MVucwnYin0aU53cuibmEKEQx056WfHyRmQMEIljOA+lioZ2lYYVO/cDUJbMUxFGUKnAdlKAxpjwBEK4CLTw67AkuFbNJSeDG1kOtU9I7xoaiVzoRMHRc4lwBnAffJSR4bWzOeDHEnLtGcvXlwIwbm+L0IN7GqZOFwhcN14KHs0yYkclO8alJ0VVkiACZvGmeI46LMoWv4b61EUgn55W2RFartJNi7cshArRfLJMqkNoAxwXe4/ElRvuk+nYtqwQyuwDC0FEAVXUntmMgnoppRqUGB++4PWdYQJtT39RoXZ3UrlA+oCrIrlpo/UPEoNBvhpmdIRK4eE6bXKvrn6PVKHhGM1GcBG3tKSdKj1x+2CgmkiUecIuJ3FKSsUPB2hXeW/i1ajzPSKkMYsM3PPGlxI="};
+    expect(passwordObj).to.not.equal(null);
+
+    done();
   });
 
   it('can create a user in mongo', function(done){
     createUser('username', 'password').then(function(user){
       expect(user.username).to.equal('username');
       expect(user.password).to.be.an('object');
-      expect(user.password.hash.length).to.equal(512);
+      expect(user.password.hash.length).to.equal(684);
+          done();
       // expect(passwordObj).to.deep.equal(expectedHashObj);
     });
-    done();
+
   });
 });
 
